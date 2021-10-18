570d206fd8d17b9eb747a3bed585aefe50e5e2c7
Jordan Scott <jscott0751@gmail.com>
2021-10-18 17:30:54 -0400

    fixed add user component

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -5,18 +5,42 @@ export default function Login() {
     return (
         <div className="col-sm-4" style={{ textAlign: 'center' }}>
             <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Group className="mb-3">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        Hello
-                    </Form.Text>
+                    <Form.Control placeholder="Username" />
+                </Form.Group>
+
+                <Form.Label>User Type</Form.Label>
+                {['checkbox'].map((type) => (
+                    <div key={`inline-${type}`} className="mb-3">
+                        <Form.Check
+                            inline
+                            label="Owner"
+                            name="group1"
+                            type={type}
+                            id={`inline-${type}-1`}
+                        />
+                        <Form.Check
+                            inline
+                            label="Promoter"
+                            name="group1"
+                            type={type}
+                            id={`inline-${type}-1`}
+                        />
+                    </div>
+                ))}
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder=" Email" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" placeholder="Password" />
                 </Form.Group>
+
+
                 <Button variant="primary" type="submit">
                     Join The Fun
                 </Button>
