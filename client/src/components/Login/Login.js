15f359bfff5bdafb17479184d4c510994de267d4
Jordan Scott <jscott0751@gmail.com>
2021-10-18 14:53:42 -0400

    added login form and component

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Form, Button } from 'react-bootstrap';
+
+export default function Login() {
+    return (
+        <div className="col-sm-4" style={{ textAlign: 'center' }}>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        Hello Fun Person
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Join The Fun
+                </Button>
+            </Form>
+        </div>
+    )
+}
