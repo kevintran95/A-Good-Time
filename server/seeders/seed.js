7531f9f83cfac411ea0d4291d0a04d0dad5ca418
Nick_Kash <nicholas.kasdjono.777@my.csun.edu>
2021-10-19 11:03:48 -0700

    resolved seed.js file

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,11 +1,17 @@
 const db = require('../config/connection');
-const { User } = require('../models');
+const { User, Event, Participant} = require('../models');
 const profileSeeds = require('./profileSeeds.json');
+const eventSeeds = require('./eventSeeds.json');
+const participantSeeds = require('./participantSeeds.json');
 
 db.once('open', async () => {
   try {
     await User.deleteMany({});
     await User.create(profileSeeds);
+    await Event.deleteMany({});
+    await Event.create(eventSeeds);
+    await Participant.deleteMany({});
+    await Participant.create(participantSeeds);
 
     console.log('all done!');
     process.exit(0);
