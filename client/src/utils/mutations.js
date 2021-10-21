f4b61540cf6cc36b64e04bbc2b879691134aa956
Jordan Scott <jscott0751@gmail.com>
2021-10-21 16:34:31 -0400

    logout buttons removes token from localstorage, and redirects to login page

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -27,8 +27,9 @@ mutation addUser ($userName: String!, $userType: String!, $email: String!, $pass
 `;
 
 export const ADD_EVENT = gql`
-mutation addEvent($eventName: String!, $eventDate: String!, $eventStart: String!, $eventEnd: String!, $eventType: String!,  $eventDescription: String!) {
-    addEvent(eventName: $eventName, eventDate: $eventDate, eventStart: $eventStart, eventEnd: $eventEnd, eventType: $eventType, eventDescription: $eventDescription) {
+mutation addEvent($promoterName: String!, $eventName: String!, $eventDate: String!, $eventStart: String!, $eventEnd: String!, $eventType: String!,  $eventDescription: String!) {
+    addEvent(promoterName: $promoterName, eventName: $eventName, eventDate: $eventDate, eventStart: $eventStart, eventEnd: $eventEnd, eventType: $eventType, eventDescription: $eventDescription) {
+      promoterName,
       eventName,
       eventDate,
       eventStart,
