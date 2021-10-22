e249d3aa8081a62e1c43f67ae1ff21e0f59130a3
Kevin Tran <kevin_tran95@yahoo.com>
2021-10-22 14:06:46 -0700

    merge

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,12 +1,6 @@
 import React from 'react'
 import { useQuery } from '@apollo/client';
-<<<<<<< HEAD
-
-// import UpcomingEvents from '../UpcomingEvents/UpcomingEvents';
-
-=======
 import UpcomingEvents from '../UpcomingEvents/UpcomingEvents';
->>>>>>> 4a5530798794975d23551e8a35f94664f112d567
 import { QUERY_EVENTS } from '../../utils/queries';
 
 
@@ -17,16 +11,6 @@ import rave from '../../assets/raveTime.mp4'
 export default function Homepage() {
     const { loading, data } = useQuery(QUERY_EVENTS);
     const events = data?.events || [];
-<<<<<<< HEAD
-
-    return (
-
-        <div>
-            <h3>Upcoming Events</h3>
-            {/* <UpcomingEvents events={events} /> */}
-        </div>
-    )
-=======
     if (loading) {
         return(
             <div>
@@ -42,5 +26,4 @@ export default function Homepage() {
             </div>
         )  
     }
->>>>>>> 4a5530798794975d23551e8a35f94664f112d567
 }
