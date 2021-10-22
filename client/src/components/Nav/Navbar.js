db5d11bc39d9baf5e611f42d0f86b281df538ab2
Nick_Kash <nicholas.kasdjono.777@my.csun.edu>
2021-10-22 14:33:46 -0700

    added style to nav bar

--- client/src/components/Nav/Navbar.js
+++ client/src/components/Nav/Navbar.js
@@ -1,9 +1,8 @@
-import { Navbar, Container, Nav } from 'react-bootstrap'
-import {
-    Link
-} from "react-router-dom";
+import {Navbar, Container, Nav} from 'react-bootstrap'
+import {Link} from "react-router-dom";
 import Auth from '../../utils/auth';
 import '../../styles/NavBar.css';
+import logo from '../../assets/agoodtimeLogo.png'
 
 export default function NavbarOne() {
     const logout = (event) => {
@@ -11,16 +10,19 @@ export default function NavbarOne() {
         Auth.logout();
     };
     return (
-        <div>
-            {Auth.loggedIn() ? (
-                <>
+        <div> {
+            Auth.loggedIn() ? (
+                <div className="top-menu">
+                    <h1 className="logo">
+                        <img src={logo} alt="logo"></img>
+                    </h1>
                     <Navbar bg="light" expand="lg">
                         <Container>
-                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                             <Navbar.Collapse id="basic-navbar-nav">
-                                <Nav className="me-auto">
+                                <Nav className="me-auto" style={{color: "black"}}>
                                     <li>
-                                        <Link to="/">Home</Link>
+                                        <Link className="color" to="/">Home</Link>
                                     </li>
                                     <li>
                                         <Link to="/schedule-event">Schedule Event</Link>
@@ -35,14 +37,17 @@ export default function NavbarOne() {
                             </Navbar.Collapse>
                         </Container>
                     </Navbar>
-                </>
+                </div>
             ) : (
-                <>
+                <div className="top-menu">
+                    <h1 className="logo">
+                        <img src={logo} alt="logo"></img>
+                    </h1>
                     <Navbar bg="light" expand="lg">
                         <Container>
-                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                             <Navbar.Collapse id="basic-navbar-nav">
-                                <Nav className="me-auto">
+                                <Nav className="me-auto" style={{color: "black"}}>
                                     <li>
                                         <Link to="/">Home</Link>
                                     </li>
@@ -62,34 +67,34 @@ export default function NavbarOne() {
                             </Navbar.Collapse>
                         </Container>
                     </Navbar>
-                </>
-            )}
-        </div>
+                </div>
+            )
+        } </div>
 
-        // <Navbar bg="light" expand="lg">
-        // <Container>
-        //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        //     <Navbar.Collapse id="basic-navbar-nav">
-        //         <Nav className="me-auto">
-        //             <li>
-        //                 <Link to="/">Home</Link>
-        //             </li>
-        //             <li>
-        //                 <Link to="/schedule-event">Schedule Event</Link>
-        //             </li>
-        //             <li>
-        //                 <Link to="/login">Login</Link>
-        //             </li>
-        //             <li>
-        //                 <Link to="/signup">Sign Up</Link>
-        //             </li>
-        //             <li>
-        //                 <button onClick={logout}>Logout</button>
-        //             </li>
-        //         </Nav>
-        //     </Navbar.Collapse>
-        // </Container>
-        // </Navbar>
+    // <Navbar bg="light" expand="lg">
+    // <Container>
+    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //     <Navbar.Collapse id="basic-navbar-nav">
+    //         <Nav className="me-auto">
+    //             <li>
+    //                 <Link to="/">Home</Link>
+    //             </li>
+    //             <li>
+    //                 <Link to="/schedule-event">Schedule Event</Link>
+    //             </li>
+    //             <li>
+    //                 <Link to="/login">Login</Link>
+    //             </li>
+    //             <li>
+    //                 <Link to="/signup">Sign Up</Link>
+    //             </li>
+    //             <li>
+    //                 <button onClick={logout}>Logout</button>
+    //             </li>
+    //         </Nav>
+    //     </Navbar.Collapse>
+    // </Container>
+    // </Navbar>
 
     )
 }
