2874610afc02279c64fe2692b852793ce8362cde
Jordan Scott <jscott0751@gmail.com>
2021-10-22 15:59:21 -0400

    changed query event to upcoming events component, but also render upcoming events on the homepage

--- client/src/components/UpcomingEvents/UpcomingEvents.js
+++ client/src/components/UpcomingEvents/UpcomingEvents.js
@@ -1,19 +1,31 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_EVENTS } from '../../utils/queries';
 
-export default function UpcomingEvents({ events }) {
-    // useEffect(() => {
-    //     console.log(events)
-    // }, [])
-    return (
-        <div>
-            <h3>Come Join The Fun</h3>
-            {events &&
-                events.map((event) => (
-                    <div key={event._id}>
-                        <h4>{event.eventName}</h4>
-                    </div>
-                ))}
-        </div>
-    );
+export default function UpcomingEvents() {
+    const { loading, data } = useQuery(QUERY_EVENTS);
+    const events = data?.events || [];
+    if (loading) {
+        return(
+            <div>
+                Loading...
+            </div>
+        )
+
+        
+    }
+    {
+        return (
+            <div>
+                <h3>Come Join The Fun</h3>
+                {events &&
+                    events.map((event) => (
+                        <div key={event._id}>
+                            <h4>{event.eventName}</h4>
+                        </div>
+                    ))}
+            </div>
+        );  
+    }
 };
 
