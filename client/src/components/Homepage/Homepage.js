81ffccde8cb41b9553dbcb1baaffc34e14dcb9a7
Jordan Scott <jscott0751@gmail.com>
2021-10-22 14:47:15 -0400

    working on upcoming events component

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css';
+import { useQuery } from '@apollo/client';
+
+import UpcomingEvents from '../UpcomingEvents/UpcomingEvents';
+
+import { QUERY_EVENTS } from '../../utils/queries';
+
 
 export default function Homepage() {
+    const { loading, data } = useQuery(QUERY_EVENTS);
+    const events = data?.events || [];
+
     return (
-        <div style={{ width: "50px" }}>
-            <Calendar />
+        <div>
+            <h3>Upcoming Events</h3>
+            <UpcomingEvents events={events} />
         </div>
     )
 }
