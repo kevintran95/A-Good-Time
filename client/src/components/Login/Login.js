5d573017086d00e7c960a1e67de1d045c55a1857
Jordan Scott <jscott0751@gmail.com>
2021-10-21 20:33:38 -0400

    centered login form

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import { Form } from 'react-bootstrap';
 import Auth from '../../utils/auth';
+import '../../styles/Login.css';
 
 import { useMutation } from '@apollo/react-hooks';
 import { LOGIN } from '../../utils/mutations'
+
 
 export default function Login() {
     const [formState, setFormState] = useState({ userName: '', password: '' });
@@ -43,34 +45,36 @@ export default function Login() {
 
     return (
         <div>
-            <Form className="col-sm-3" style={{ margin: 0, textAlign: "center" }}>
-                <Form.Group className="mb-3">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control
-                        placeholder="Your userName"
-                        name="userName"
-                        type="userName"
-                        value={formState.userName}
-                        onChange={handleChange} />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formGroupPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        placeholder="Your Password"
-                        name="password"
-                        type="password"
-                        value={formState.password}
-                        onChange={handleChange} />
-                </Form.Group>
-                <button
-                  className="btn btn-block btn-info"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                  onClick = { handleFormSubmit }
-                >
-                  Submit
-                </button>
-            </Form>
+            <div>
+                <Form className="col-sm-3">
+                    <Form.Group className="mb-3">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control
+                            placeholder="Your userName"
+                            name="userName"
+                            type="userName"
+                            value={formState.userName}
+                            onChange={handleChange} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroupPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            placeholder="Your Password"
+                            name="password"
+                            type="password"
+                            value={formState.password}
+                            onChange={handleChange} />
+                    </Form.Group>
+                    <button
+                        className="btn btn-block btn-info"
+                        style={{ cursor: 'pointer' }}
+                        type="submit"
+                        onClick={handleFormSubmit}
+                    >
+                        Submit
+                    </button>
+                </Form>
+            </div>
         </div>
     )
 }
